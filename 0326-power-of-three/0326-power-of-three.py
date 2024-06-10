96d32560a22bc26b4ad9f299745e0cf0233aabbf
Dharma4641 <115545357+Dharma4641@users.noreply.github.com>
2024-06-10 21:49:07 +0530

    Time: 75 ms (19.42%), Space: 11.7 MB (32.01%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,9 +1,14 @@
-import math
 class Solution(object):
     def isPowerOfThree(self, n):
+        def rec(n):
+            if n==1:
+                return True
+            if n%3==0:
+                return rec(n//3)
+            else:
+                return False
         if n<=0:
             return False
-        res=math.log10(n)/math.log10(3)
-        return res.is_integer()
+        return rec(n)
             
         
