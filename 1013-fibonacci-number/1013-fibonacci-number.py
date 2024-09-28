c01ccad681d6acbed736104a9bb58157ea4e2546
Dharma4641 <115545357+Dharma4641@users.noreply.github.com>
2024-09-28 16:36:41 +0530

    Time: 400 ms (5.47%), Space: 16.5 MB (59.62%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,12 +1,8 @@
-class Solution(object):
-    def fib(self, n):
-        def fibo(n):
-            if n==0:
-               return 0
-            elif n==1:
-                return 1
-            elif n==2:
-                return 1
-            return fibo(n-1) + fibo(n-2)
-        return fibo(n)
-        
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        return self.fib(n-1)+self.fib(n-2)
+
